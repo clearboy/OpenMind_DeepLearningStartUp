3cb6590928be751f442acd7bcb214aa872a3b79b
Xiaoyu Wang <xywang@xywang.org>
2017-10-04 15:45:14 +0800

    split the words and form the dict

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OpenMind_DeepLearningStartUp
 Learn DeepLearningStartUp in OpenMind
+by 王晓钰
+
+@ 2017年10月4日
--- count.py
+++ count.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import numpy as np
+#import numpy as np
+import re
 
 filename = "happiness_seg.txt"
-filename = "happiness.txt"
+filename = "happiness.txt"  # for test
 
-file_object = open(filename) #不要把open放在try中，以防止打开失败，那么就不用关闭了
+file_object = open(filename,'r',encoding="UTF-8") 
 
 try:
-    file_context = file_object.read() #file_context是一个string，读取完后，就失去了对test.txt的文件引用
-    #  file_context = open(file).read().splitlines() 
-    # file_context是一个list，每行文本内容是list中的一个元素
+    file_context = file_object.read() # 文件注释
 finally:
     file_object.close()
-# 除了以上方法，也可用with、contextlib都可以打开文件，且自动关闭文件，
-# 以防止打开的文件对象未关闭而占用内存
 
-print file_context
+print(file_context)
 
-print len(file_context)
+print(len(file_context))
 
-words = file_context.split()
+
+'''
+words = file_context.split() #) # ('，|,|; |\*|\n|\l| ')
 
 for word in words:
-    print '%d:%s' %(len(word),word)
+    print ('%d:%s' %(len(word),word))
+
+'''
+
+print("==================")
+words2 = re.split(r'[： 。 ； ， ： “ ”（ ） 、 ？ 《 》 \s \t]',file_context)
+
+arrayWord = {}
+index = 0
+for word in words2:
+    if len(word)>0:
+        if word in arrayWord:
+            arrayWord[word] = arrayWord[word] +1
+        else:
+            arrayWord[word] = 1
+        
+    print ('%d:%s' %(len(word),word))
+    
+
+print(arrayWord)
 
