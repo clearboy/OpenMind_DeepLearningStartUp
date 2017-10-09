cad8282b1153519d615b1668858062747766a931
Xiaoyu Wang <xywang@xywang.org>
2017-10-09 13:01:17 +0800

    arr<0

--- advanced2.py
+++ advanced2.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 arr = np.random.normal(size=100)
 print(arr)
+arr[arr<0]=0
 plt.hist(arr, bins=100, normed=1)
 plt.show()
 
-from pylab import *
-hist(arr)
-show()
+# from pylab import *
+#hist(arr)
+# show()
